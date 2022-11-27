b87a48d42f50acdd846187a69504730ea20a0b82
Matthew Kim <matthew.kim.733@my.csun.edu>
2022-11-26 16:07:18 -0800

    fixed cls issue and styled more font sizes

--- Eye Candy Project/Code Design/Front-End/eye-candy/src/Pages/Home.js
+++ Eye Candy Project/Code Design/Front-End/eye-candy/src/Pages/Home.js
@@ -3,15 +3,19 @@ import "./Home.css";
 
 function Home() {
   const myRef = useRef();
-  const img = useRef();
+  const img1 = useRef();
+  const img2 = useRef();
+  const img3 = useRef();
+
   const [isVisible, setIsVisible] = useState();
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries, observer) => {
         const [entry] = entries;
+        if (!entry.isIntersecting) return;
         entry.target.classList.remove("section--hidden");
-        observer.unobserve(entry.target);
+        observer.unobserve(entry);
       },
       {
         root: null,
@@ -36,33 +40,37 @@ function Home() {
           <img
             src={require("../Assets/landing-page-photo.jpg")}
             alt="logo"
-            className="landing-photo"
-            ref={img}
+            className="img--section landing-photo"
+            ref={img1}
+            width="722"
+            height="481"
+            loading="lazy"
           />
           <img
             src={require("../Assets/landing-page-photo2.jpg")}
             alt="logo"
-            className="landing-photo2"
-            ref={img}
+            className="img--section landing-photo2"
+            width="361"
+            height="542"
+            ref={img2}
           />
           <img
             src={require("../Assets/landing-page-photo3.jpg")}
             alt="logo"
-            className="landing-photo3"
-            ref={img}
+            className="img--section landing-photo3"
+            width="252"
+            height="379"
+            ref={img3}
           />
         </div>
       </div>
-
       <section>
         <div className="section" id="section--1"></div>
         <div className="features"></div>
       </section>
-
       <section class="section" id="section--2">
         <div className="operations"></div>
       </section>
-
       <section className="section" id="section--3">
         <div className="section__title section__title--testimonials"></div>
         <div className="slider"></div>
