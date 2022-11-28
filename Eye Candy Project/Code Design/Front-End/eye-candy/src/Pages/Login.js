951cb7f83b6cfc9b7174d10b6c185a0c5de6ddc1
Andres <AndresEM1524@gmail.com>
2022-11-27 20:47:49 -0800

    Added login page with basic structure as well some css

--- Eye Candy Project/Code Design/Front-End/eye-candy/src/Pages/Login.js
+++ Eye Candy Project/Code Design/Front-End/eye-candy/src/Pages/Login.js
@@ -1,12 +1,20 @@
 import React from 'react'
-
+import './Login.css';
 
 function Login() {
   return (
-    <div>
-        <h1>Login</h1>
-        <form action=""></form>
-    </div>
+      <div className="login-content">
+        <div className="login-container">
+        <form action="">
+          <label htmlFor="userName">Username: </label> <br />
+          <input type="text" className="userName" placeholder='username'/> <br />
+          <label htmlFor="password">Password: </label> <br />
+          <input type="password" className="password" /> <br />
+          <p>Don't have an account? Click <a href="">here</a> </p>
+          <button type='submit'>Login</button>
+        </form>
+        </div>
+        </div>
   )
 }
 
