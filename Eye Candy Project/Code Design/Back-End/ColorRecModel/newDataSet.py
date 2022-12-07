68407243aa15c29b6ef8525e0222016d22ed9d21
Kennedy Ashley Johnson <kenn.john320@gmail.com>
2022-12-07 15:40:08 -0800

    Create newDataSet.py
    not completed/based on chanel_dataset.py

---
+++ Eye Candy Project/Code Design/Back-End/ColorRecModel/newDataSet.py
@@ -0,0 +1,37 @@
+# gets Dataset class from Pytorch
+from torch.utils.data import Dataset
+# PIL (Python Image Library) that will be used to handle images
+from PIL import Image 
+# torch transform used for computer vision applications (i.e helps computer seen the image)
+import torchvision.transforms as transforms 
+
+import torch
+import glob
+
+# Class labels by ModaNet
+''' 
+    0 - background
+    1 - bag (purse/etc) 
+    2 - belt
+    3 - boots
+    4 - footwear (sandals/sneakers/etc)
+    5 - outerwear (jacket/sweater/vest/coat/etc)
+    6 - dress
+    7 - sunglasses
+    8 - pants (leggings/jeans)
+    9 - top (blouse/t-shirt/shirt)
+   10 - shorts
+   11 - skirt
+   12 - headwear
+   13 - scarf/tie
+'''
+
+def get_class_names_md():
+    ModaNet_class_names = ['background', 'bag','belt','boots','footwear','outer',
+                            'dress','sunglasses','pants', 'top','shorts','skirts',
+                            'headwear','scrtf-tie']
+    return ModaNet_class_names
+
+def get_class_names():
+    class_names = []
+    return class_names
