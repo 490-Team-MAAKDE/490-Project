84e76603a5a55bee8c8aad477b7344629fa682ea
Kennedy Ashley Johnson <kenn.john320@gmail.com>
2022-12-11 14:43:59 -0800

    Update newDataSet.py

--- Eye Candy Project/Code Design/Back-End/ColorRecModel/newDataSet.py
+++ Eye Candy Project/Code Design/Back-End/ColorRecModel/newDataSet.py
@@ -33,5 +33,11 @@ def get_class_names_md():
     return ModaNet_class_names
 
 def get_class_names():
-    class_names = []
+    class_names = ['background','accessories','bag','belt','blazer','blouse','bodysuit',
+    'boots','bra','bracelet','cape','cardigan','clogs','coat','dress','earrings','flats',
+    'glasses','gloves','hair','hat','heels','hoodie','intimate','jacket','jeans','jumper',
+    'leggings','loafers','necklace','panties','pants','pumps','purse','ring','romper','sandals',
+    'scarf','shirt','shoes','shirt','shoes','shorts','skin','skirt','sneakers','socks','stockings',
+    'suit','sunglasses','sweater','sweatshirt','swimswear','t-shirt','tie','tights','top','vest',
+    'vest','wallet','watch','wedges']
     return class_names
