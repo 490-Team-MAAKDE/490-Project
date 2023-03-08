b82fd99c4a5c7e3e462ec68faa5d32ba4df8a6b1
Matthew <matthewkimcs@gmail.com>
2023-03-07 21:40:27 -0800

    small git ignore change

--- Eye Candy Project/Code Design/Front-End/eye-candy/src/Pages/Login.js
+++ Eye Candy Project/Code Design/Front-End/eye-candy/src/Pages/Login.js
@@ -1,45 +1,47 @@
-import React, { useState } from 'react'
-import Axios from 'axios'
+import React, { useState } from "react";
+import Axios from "axios";
 import "./Login.css";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState("");
 
-const [username, setUsername] = useState('');
-const [password, setPassword] = useState('');
-
-const [loginStatus, setLoginStatus] = useState('');
-
-const login = () =>{
-  Axios.post ('http://localhost:3001/login', {
+  const login = (e) => {
+    e.preventDefault();
+    Axios.post("http://localhost:3001/login", {
       username: username,
       password: password,
-  }).then((response ) =>{
+    }).then((response) => {
+      if (response.data.message) {
+        setLoginStatus(response.data.message);
+      } else {
+        setLoginStatus(response.data[0].username);
+      }
+    });
+  };
 
-    if (response.data.message){
-      setLoginStatus(response.data.message)
-    }
-    else {
-      setLoginStatus(response.data[0].username);
-  }
-  });
-};
-
+  console.log(123);
   return (
     <div className="login-content">
       <div className="login-container">
         <form action="">
           <label htmlFor="userName">Username: </label> <br />
           <input
-                type="text"
-                onChange={(e) => {setUsername(e.target.value)}}
-                />{" "}
-                <br />
+            type="text"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />{" "}
+          <br />
           <label htmlFor="password">Password: </label> <br />
           <input
-                type="text"
-                onChange={(e) => {setPassword(e.target.value)}}
-                />{" "}
-                <br />
+            type="text"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />{" "}
+          <br />
           <p>
             Don't have an account? Click <a href="">here</a>
           </p>
