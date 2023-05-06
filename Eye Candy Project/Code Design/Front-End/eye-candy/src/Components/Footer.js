35c78066bdf44fa6695f09b7149c9586eb19c652
Andres <AndresEM1524@gmail.com>
2023-05-05 19:59:27 -0700

    Additional removal of code for font-awesome

--- Eye Candy Project/Code Design/Front-End/eye-candy/src/Components/Footer.js
+++ Eye Candy Project/Code Design/Front-End/eye-candy/src/Components/Footer.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Footer.css'
 
-<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-    
-</link>
 function Footer() {
   return (
     
