0b308fb54e602145f636a1efc6fa9a315d1fe278
Aaron Gian Madrid <aaron.gl.madrid@gmail.com>
2023-05-06 14:27:51 -0700

    Co-authored-by: Deni Cabaravdic <d3h1@users.noreply.github.com>

--- Eye Candy Project/Code Design/Back-End/ImageRecModel/app.py
+++ Eye Candy Project/Code Design/Back-End/ImageRecModel/app.py
@@ -1,113 +1,87 @@
-# Needed imports based on pip installs
-from flask import Flask, request, jsonify
-from PIL import Image
-import tensorflow as tf
-from keras.models import load_model
-import numpy as np
-from flask_cors import CORS
 import os
 import base64
+from flask import Flask, request, jsonify
+from PIL import Image
+import numpy as np
+from keras.models import load_model
+from flask_cors import CORS
 from werkzeug.utils import secure_filename
-
-import logging
-logging.basicConfig(level=logging.DEBUG)
 
 # Load the model outside the predict function
 model = load_model('clothing_classifier_model_v2.h5')
 
-# This will start our instance
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-# This will enable cors that allows us to connect our flask to react
-CORS(app, resources={r"/predict": {"origins": "http://localhost:3000"}})
+CORS(app, origins="*", allow_headers=["Content-Type"])
 
-# We are setting max size of file as 10mb
-app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
-
-# Our upload folder will be designated here and the allowed extensions as well
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'Test Images')
+UPLOAD_FOLDER = 'static/images'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# This function checks if the file uploaded is valid
+# Create the UPLOAD_FOLDER if it doesn't exist
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 def allowed_files(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# This function will pull the model and predict based on different classes
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        print("Entered predict function")
-        # This ensure file path is not empty
-        if 'file' not in request.files:
-            return jsonify({'error': 'No file part'}), 400
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
 
-        file = request.files['file']
+    file = request.files['file']
 
-        if file.filename == '':
-            return jsonify({'error': 'No selected file'}), 400
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
 
-        # So once the file path is determined -- We will convert images
-        if file and allowed_files(file.filename):
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
+    if file and allowed_files(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
 
-            print(f"Attempting to open file: {file_path}")
-            # They will convert to 28x28 grayscale images
-            img = Image.open(file_path).convert('L')
-            img = img.resize((28, 28), Image.ANTIALIAS)
-            img = np.array(img)
-            img = (255 - img) / 255.0
-            img = np.expand_dims(img, axis=0)
-            img = np.expand_dims(img, axis=-1)
+        img = Image.open(file_path).convert('L')
+        img = img.resize((28, 28), Image.ANTIALIAS)
+        img = np.array(img)
+        img = (255 - img) / 255.0
+        img = np.expand_dims(img, axis=0)
+        img = np.expand_dims(img, axis=-1)
 
-            class_prediction = model.predict(img)
-            predicted_label = int(np.argmax(class_prediction))
+        class_prediction = model.predict(img)
+        predicted_label = int(np.argmax(class_prediction))
 
-            # These are the classes that will be used to determine the items uploaded
-            if predicted_label == 0:
-                product = "TShirt/top"
-            elif predicted_label == 1:
-                product = "Trouser"
-            elif predicted_label == 2:
-                product = "Pullover"
-            elif predicted_label == 3:
-                product = "Dress"
-            elif predicted_label == 4:
-                product = "Coat"
-            elif predicted_label == 5:
-                product = "Sandal"
-            elif predicted_label == 6:
-                product = "Shirt"
-            elif predicted_label == 7:
-                product = "Sneaker"
-            elif predicted_label == 8:
-                product = "Bag"
-            else:
-                product = "Ankle Boot"
+        if predicted_label == 0:
+            product = "TShirt/top"
+        elif predicted_label == 1:
+            product = "Trouser"
+        elif predicted_label == 2:
+            product = "Pullover"
+        elif predicted_label == 3:
+            product = "Dress"
+        elif predicted_label == 4:
+            product = "Coat"
+        elif predicted_label == 5:
+            product = "Sandal"
+        elif predicted_label == 6:
+            product = "Shirt"
+        elif predicted_label == 7:
+            product = "Sneaker"
+        elif predicted_label == 8:
+            product = "Bag"
+        else:
+            product = "Ankle Boot"
 
-            # This will allow us to upload images and show them properly on the front end page using base64 string formatting
-            with open(file_path, "rb") as f:
-                user_image = base64.b64encode(f.read()).decode("utf-8")
+        with open(file_path, "rb") as f:
+            user_image = base64.b64encode(f.read()).decode("utf-8")
 
-            os.remove(file_path)
+        os.remove(file_path)
 
-            # This will use JSONIFY to generate our output and the image, the same that we will use with our color model
-            return jsonify({
-                'product': product,
-                'user_image': f"data:image/jpeg;base64,{user_image}",
-            })
+        return jsonify({
+            'product': product,
+            'user_image': f"data:image/jpeg;base64,{user_image}",
+        })
 
-        # Outputs error messages if something is input, uploaded, or mismatched wrong
-        return jsonify({'error': 'Invalid file extension'}), 400
-    except Exception as e:
-        # Log the error and return a 500 response
-        app.logger.error(str(e))
-        return jsonify({'error': 'Internal Server Error'}), 500
-    
-# The way our flask app runs, runs with NPM RUN START:BOTH
+    return jsonify({'error': 'Invalid file extension'}), 400
+
 if __name__ == '__main__':
     app.run(debug=True)
--- Eye Candy Project/Code Design/Front-End/eye-candy/src/Pages/Login.js
+++ Eye Candy Project/Code Design/Front-End/eye-candy/src/Pages/Login.js
@@ -4,25 +4,25 @@ import React, { useEffect, useState } from "react";
 import { Button } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
 import Axios from "axios";
+import { Navigate } from "react-router-dom";
 import "./Login.css";
 
 //This is the Sign Up Funtion for the overall page
 function Login() {
-  /*This is the useState to set the value of the stated 
+    /*This is the useState to set the value of the stated 
   variables from whatever the user inputs in the text boxes.*/
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loginStatus, setLoginStatus] = useState("");
+  const [navigateToDashboard, setNavigateToDashboard] = useState(false);
 
-  //Allow for cookies to be stored to the application
+   //Allow for cookies to be stored to the application
   Axios.defaults.withCredentials = true;
 
   /*This portion obtains the values from user input and 
   place them in\in variables to be verified with the SQL 
   Database*/
   const login = (e) => {
-    /*Temporarily palced this since the page automatically 
-    reroutes after submit*/
     e.preventDefault();
 
     //Axios POST will be for inserting data when logging in
@@ -34,21 +34,28 @@ function Login() {
         setLoginStatus(response.data.message);
       } else {
         setLoginStatus(response.data[0].username);
+        setNavigateToDashboard(true);
       }
     });
   };
 
-  useEffect(() => {
-    /*Axios GET will serve as getting information when there is
+  /*Axios GET will serve as getting information when there is
      page refresh. This portion will check for cookies and see
      if there are any saved ones and refer to and keep login 
      status*/
+  useEffect(() => {
     Axios.get("http://localhost:3001/login").then((response) => {
-      if (response.data.loggedIn == true) {
+      if (response.data.loggedIn === true) {
         setLoginStatus(response.data.user[0].username);
+        setNavigateToDashboard(true);
       }
     });
   }, []);
+
+  //This should redirect you if login is usccessful
+  if (navigateToDashboard) {
+    return <Navigate to="/dashboard" />;
+  }
 
   /*Display the form for users to input their login credentials.
   This will compare with SQL and determine authenticity*/
@@ -67,7 +74,7 @@ function Login() {
           </div>
           <br />
           <div>
-            <label htmlFor="password">Password: </label> <br />
+            <label htmlFor="password" typeof="password">Password: </label> <br />
             <Input
               type="text"
               onChange={(e) => {
@@ -84,9 +91,6 @@ function Login() {
           </Button>
         </form>
       </div>
-      {/* loginStatus plays a roll in currently displaying 
-      login success/failure message. Refer to login.js[29-39]
-      as well as database.js [75-105] */}
       <h2>{loginStatus}</h2>
     </div>
   );
