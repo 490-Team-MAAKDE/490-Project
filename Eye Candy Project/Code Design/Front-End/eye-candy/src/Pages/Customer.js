eda80dd17ca19aacd6fc791afe98d076138a7bdb
Matthew <matthewkimcs@gmail.com>
2023-05-09 19:33:33 -0700

    setup user db connection

---
+++ Eye Candy Project/Code Design/Front-End/eye-candy/src/Pages/Customer.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+function Customer() {
+  const [user, setUser] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    async function fetchUsers() {
+      const payload = await fetch(`http://localhost:3001/customer/${id}`);
+      const data = await payload.json();
+      console.log(data);
+      setUser(data);
+    }
+    fetchUsers();
+  }, [id]);
+  return (
+    <div>
+      {!!user.length &&
+        user.map((obj) => {
+          return (
+            <div key={obj.id}>
+              <span>Username: {obj.username}</span>
+              <span>Firstname: {obj.firstname}</span>
+              <span>Lastname: {obj.lastname}</span>
+              <span>Email: {obj.email}</span>
+            </div>
+          );
+        })}
+    </div>
+  );
+}
+
+export default Customer;
